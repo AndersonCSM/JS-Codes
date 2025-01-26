9be716fd9516e6cef27a79d60e59af579142bbd3
Anderson Carlos <96142983+AndersonCSM@users.noreply.github.com>
2025-01-26 15:27:27 -0300

    =

--- DIO/Formacao-Logica-Programacao/exe-03.js
+++ DIO/Formacao-Logica-Programacao/exe-03.js
@@ -1,36 +1,19 @@
-class hero{
-    constructor(nome, idade, tipo){
-        this.nome = nome;
-        this.idade = idade;
-        this.tipo = tipo;
-    }
+//Desafios JavaScript na DIO têm funções "gets" e "print" acessíveis globalmente:
+//- "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
+//- "print": imprime um texto de saída (output), pulando linha.
 
-    ataque_acc(){
-        switch (this.tipo){
-            case "mago":
-                this.ataque = "magia";
-            break;
+// Lê a quantidade de golpes informada pelo usuário. 
+// O parseInt(()) vai converter os valores de entrada(string) para um valor numérico(Int).
+const quantidadeGolpes = 4;
 
-            case "guerreiro":
-                this.ataque = "espada";
-            break;
+// TODO: Defina aqui os tipos de minerais Carvao, Ferro, Diamante e Pedra
+let minerais = ["Carvao", "Ferro", "Diamante", "Pedra"];
 
-            case "monge":
-                this.ataque = "artes marciais";
-            break;
-
-            case "ninja":
-                this.ataque = "shuriken";
-            break;
-
-            default:
-                this.ataque = "Pedra";
-        }
-
-        return this.ataque;
-    }
+// Loop para cada golpe, de 1 até a quantidade informada
+for (let i = 1; i <= quantidadeGolpes; i++) {
+  // Calcula o índice do mineral usando o operador de módulo (%) para garantir que o índice esteja dentro do tamanho do array
+  let minaIndex = i % minerais.length;
+  
+  // TODO: Agora exiba o índice i, concatene com o caractere ":", após, concatene com tipo de minerais[minaIndex]:
+  console.log(i+": "+minerais[minaIndex]);
 }
-
-let heroi1 = new hero("Anakron", 23, "guerreiro");
-
-console.log(`O ${heroi1.tipo} atacou usando ${heroi1.ataque_acc()}`);
